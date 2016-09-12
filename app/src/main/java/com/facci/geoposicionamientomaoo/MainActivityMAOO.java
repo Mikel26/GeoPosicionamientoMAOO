290e2d05c1fda3de27153273777961e163b0b103
Mikel <mikel.ortega26@gmail.com>
2016-09-12 09:17:01 -0500

    GeoPosicionamiento GPS
    App Movil para captura de coordenadas mediante GPS

---
+++ app/src/main/java/com/facci/geoposicionamientomaoo/MainActivityMAOO.java
@@ -0,0 +1,85 @@
+package com.facci.geoposicionamientomaoo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivityMAOO extends AppCompatActivity {
+
+    LocationManager locManager;
+
+    private double latitud;
+    private double longitud;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity_maoo);
+
+        locManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+        List<String> listaProviders = locManager.getAllProviders();
+
+        LocationProvider provider = locManager.getProvider(listaProviders.get(0));
+
+        int precision = provider.getAccuracy();
+        boolean obtenerAltitud = provider.supportsAltitude();
+        int consumoRecursos = provider.getPowerRequirement();
+        Toast.makeText(MainActivityMAOO.this, String.format("Precisión: %s \n Consumo Recursos: %s",
+                String.valueOf(precision),String.valueOf(obtenerAltitud),String.valueOf(consumoRecursos)), Toast.LENGTH_LONG).show();
+
+    }
+
+    public void ActualizarCoordenadasClick (View v){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+        PackageManager.PERMISSION_GRANTED){
+            locManager.requestLocationUpdates(locManager.GPS_PROVIDER, 2*60*1000, 10, locationListenerGPS);
+        }
+    }
+
+    private final LocationListener locationListenerGPS = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            longitud = location.getLongitude();
+            latitud = location.getLatitude();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    EditText txtLongitud = (EditText) findViewById(R.id.txtLongitud);
+                    EditText txtLatitud = (EditText) findViewById(R.id.txtLatitud);
+                    txtLongitud.setText(longitud + "");
+                    txtLatitud.setText(latitud + "");
+                    Toast.makeText(MainActivityMAOO.this, "Coordenadas GPS Capturadas", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+}
